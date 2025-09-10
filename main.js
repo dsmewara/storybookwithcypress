30fa3ee8b693bcd08179913785e2613eae52979b
Dalpat Singh Mewara <dsmewara@users.noreply.github.com>
2025-09-10 19:18:37 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,47 +1,20 @@
-const path = require("path");
-
-// Helper function to get absolute path
-function getAbsolutePath(pkg) {
-  return path.dirname(require.resolve(path.join(pkg, "package.json")));
-}
-
-module.exports = {
-  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx|mdx)"],
-  addons: [
-    {
-      name: "@storybook/addon-docs",
-      options: { transcludeMarkdown: true },
-    },
-    "@storybook/addon-essentials",
-    "@chromatic/storybook"
-  ],
-  framework: {
-    name: "@storybook/react-webpack5",
-    options: {},
-  },
-  docs: {
-    autodocs: true,
-  },
-  typescript: {
-    reactDocgen: "react-docgen-typescript",
-  },
-  webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      use: [
-        {
-          loader: require.resolve("babel-loader"),
-          options: {
-            presets: [
-              require.resolve("@babel/preset-env"),
-              [require.resolve("@babel/preset-react"), { runtime: "automatic" }],
-              require.resolve("@babel/preset-typescript"),
-            ],
-          },
+webpackFinal: async (config) => {
+  config.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    exclude: /\.d\.ts$/, // ✅ exclude type declarations
+    use: [
+      {
+        loader: require.resolve("babel-loader"),
+        options: {
+          presets: [
+            require.resolve("@babel/preset-env"),
+            [require.resolve("@babel/preset-react"), { runtime: "automatic" }],
+            require.resolve("@babel/preset-typescript"),
+          ],
         },
-      ],
-    });
-    config.resolve.extensions.push(".ts", ".tsx");
-    return config;
-  },
-};
+      },
+    ],
+  });
+  config.resolve.extensions.push(".ts", ".tsx");
+  return config;
+},
